30ade9004525498e4afb589bed1f5f3af99d9ec3
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-01-24 19:57:57 +0100

    Updated LogOut.

--- src/components/LogOut/LogOut.tsx
+++ src/components/LogOut/LogOut.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSessionStorage } from "@uidotdev/usehooks";
 
+import globalState from "@/state/state";
 import { PATHS } from "@/constants";
 import { UserData } from "@/types/user-data.types";
 
@@ -17,6 +18,12 @@ const LogOut = ({ children, ...props }: ILogOutProps): JSX.Element => {
   const [, setUserData] = useSessionStorage<UserData>("user-data", null);
 
   const onLogOutButtonClick = (): void => {
+    globalState.set({
+      userData: {
+        jwt: "",
+        role: "",
+      },
+    });
     setUserData(null);
     navigate(`${PATHS.AUTH}/${PATHS.LOGIN}`);
   };
